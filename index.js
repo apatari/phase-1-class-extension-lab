91ee2b4265e4753c47b5724949f75e4e250a9357
Andy Patari <andy.patari@gmail.com>
2023-08-01 13:44:29 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+class Polygon {
+    constructor(arr) {
+        this.arr = arr
+    }
+
+    get countSides() {
+        return this.arr.length
+    }
+
+    get perimeter() {
+        return this.arr.reduce((acc, val) => acc + val, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        for(let i = 0; i < this.arr.length; i++){           
+            if(this.arr[i] > sumArray(this.arr) - this.arr[i]) {
+                return false
+            }            
+        }
+        return true
+    }
+}
+
+function sumArray(arr) {
+    return arr.reduce((acc, val) => acc + val, 0)
+}
+
+class Square extends Polygon {
+    get isValid() {
+        for(let side of this.arr) {
+            if(side != this.arr[0]) {
+                return false
+            }
+        }
+        return true
+    }
+    get area() {
+        return this.arr[0] ** 2
+    }
+}
